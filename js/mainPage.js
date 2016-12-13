4f71971ad87b41cb2597e47ed3148138d4f4e203
Evan <calliaevan12@gmail.com>
2016-12-13 11:30:20 -0800

    animated scrolling

---
+++ js/mainPage.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    // Scroll functionality
+    $('body').scrollspy({target: ".navbar", offset: 50});
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+
+});
